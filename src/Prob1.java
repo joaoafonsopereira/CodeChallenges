fc7c000eb41e7d2c92935275ae2ebfd06dccf2a6
Joao Pereira <joaop6775@gmail.com>
2020-09-08 17:52:33 +0100

    Initial commit - problems 1 & 2 solved

---
+++ src/Prob1.java
@@ -0,0 +1,16 @@
+public class Prob1 {
+
+    static boolean isMultiple(int x, int y) {
+        return (x % y) == 0;
+    }
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1; i < 1000; i++) {
+            if(isMultiple(i, 3) || isMultiple(i, 5))
+                sum += i;
+        }
+        System.out.println(sum);
+    }
+
+}
---
+++ src/Prob2.java
@@ -0,0 +1,20 @@
+public class Prob2 {
+
+    public static void main(String[] args) {
+
+        int fn2 = 0;
+        int fn1 = 1;
+        int fn = fn1 + fn2;
+        int sum = 0;
+
+        while(fn < 4000000) {
+            fn = fn1 + fn2;
+            if(fn % 2 == 0)
+                sum += fn;
+            fn2 = fn1;
+            fn1 = fn;
+        }
+        System.out.println(sum);
+    }
+
+}
