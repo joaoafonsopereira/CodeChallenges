1cac1c60b6abcb346233a9ef79ca6092676ad50e
Joao Pereira <jao.pereira@campus.fct.unl.pt>
2020-09-14 21:16:10 +0100

    Solved problem 5

---
+++ src/main/java/Prob5.java
@@ -0,0 +1,19 @@
+import static primes.Primes.isMultiple;
+
+public class Prob5 {
+
+    static boolean divisibleBy1to20(int n) {
+        for(int d = 2; d <= 20; d++) {
+            if(!isMultiple(n, d))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int res = 2520;
+        while(!divisibleBy1to20(res))
+            res += 2;
+        System.out.println(res);
+    }
+}
