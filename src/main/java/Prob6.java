c4b542164de87b235f2fb07672b69b363abc8729
Joao Pereira <jao.pereira@campus.fct.unl.pt>
2020-09-15 12:04:48 +0100

    Solved probs 6 and 7

---
+++ src/main/java/Prob6.java
@@ -0,0 +1,14 @@
+public class Prob6 {
+
+    public static void main(String[] args) {
+        int sumOfSquares = 0;
+        int squareOfSum = 0;
+        for(int i = 1; i <= 100; i++) {
+            sumOfSquares += i*i;
+            squareOfSum += i;
+        }
+        squareOfSum *= squareOfSum;
+        int dif = squareOfSum - sumOfSquares;
+        System.out.println("Dif = " + dif);
+    }
+}
---
+++ src/main/java/Prob7.java
@@ -0,0 +1,9 @@
+import primes.Primes;
+
+public class Prob7 {
+
+    public static void main(String[] args) {
+        long p = Primes.getNthPrime(10001);
+        System.out.println(p);
+    }
+}
